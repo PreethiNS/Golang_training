d93d3347fa082c1ec602680c3101e894520ee9a9
PreethiNS <preethinskote@gmail.com>
2023-12-12 15:23:35 +0530

    day-14 main file

---
+++ Day-14/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	context "context"
+	"log"
+	"net"
+
+	pb "github.com/PreethiNS/day-14/main"
+	grpc "google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
